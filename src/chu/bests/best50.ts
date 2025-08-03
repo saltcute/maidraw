93f97ad1731a87e6759375ff376472fa9c41c72c
potatopotat0 <xuanjiap@lolicon.ac.cn>
2025-08-04 00:48:40 +0800

    chore: remove redundant code

--- src/chu/bests/best50.ts
+++ src/chu/bests/best50.ts
@@ -25,52 +25,6 @@ import * as kamaiTachi from "../lib/kamaiTachi";
 import * as lxns from "../lib/lxns";
 import { Util } from "@maidraw/lib/util";
 
-class HalfFullWidthConvert {
-    private static readonly charsets = {
-        latin: { halfRE: /[!-~]/g, fullRE: /[！-～]/g, delta: 0xfee0 },
-        hangul1: { halfRE: /[ﾡ-ﾾ]/g, fullRE: /[ᆨ-ᇂ]/g, delta: -0xedf9 },
-        hangul2: { halfRE: /[ￂ-ￜ]/g, fullRE: /[ᅡ-ᅵ]/g, delta: -0xee61 },
-        kana: {
-            delta: 0,
-            half: "｡｢｣､･ｦｧｨｩｪｫｬｭｮｯｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝﾞﾟ",
-            full:
-                "。「」、・ヲァィゥェォャュョッーアイウエオカキクケコサシ" +
-                "スセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワン゛゜",
-        },
-        extras: {
-            delta: 0,
-            half: "¢£¬¯¦¥₩\u0020|←↑→↓■°",
-            full: "￠￡￢￣￤￥￦\u3000￨￩￪￫￬￭￮",
-        },
-    };
-    // @ts-ignore
-    private static readonly toFull = (set) => (c) =>
-        set.delta
-            ? String.fromCharCode(c.charCodeAt(0) + set.delta)
-            : [...set.full][[...set.half].indexOf(c)];
-    // @ts-ignore
-    private static readonly toHalf = (set) => (c) =>
-        set.delta
-            ? String.fromCharCode(c.charCodeAt(0) - set.delta)
-            : [...set.half][[...set.full].indexOf(c)];
-    // @ts-ignore
-    private static readonly re = (set, way) =>
-        set[way + "RE"] || new RegExp("[" + set[way] + "]", "g");
-    private static readonly sets = Object.values(this.charsets);
-    // @ts-ignore
-    static toFullWidth = (str0) =>
-        this.sets.reduce(
-            (str, set) => str.replace(this.re(set, "half"), this.toFull(set)),
-            str0
-        );
-    // @ts-ignore
-    static toHalfWidth = (str0) =>
-        this.sets.reduce(
-            (str, set) => str.replace(this.re(set, "full"), this.toHalf(set)),
-            str0
-        );
-}
-
 interface ITheme {
     manifest: IThemeManifest;
     path: string;
@@ -898,7 +852,7 @@ export class Best50 {
 
             Util.drawText(
                 ctx,
-                HalfFullWidthConvert.toFullWidth(username),
+                Util.HalfFullWidthConvert.toFullWidth(username),
                 element.x + element.height * (56 / 64),
                 element.y + element.height * (0.3 + 1 / 4),
                 (element.height * 1) / 8,
@@ -1101,11 +1055,12 @@ export class Best50 {
                             return (
                                 scores
                                     .map((v) => v.rating)
-                                    .reduce((sum, v) => (sum += v)) / length
+                                    .reduce((sum, v) => sum + v, 0) / length
                             );
                         }
                         await this.drawTextModule(ctx, currentTheme, element, {
-                            username: HalfFullWidthConvert.toFullWidth(name),
+                            username:
+                                Util.HalfFullWidthConvert.toFullWidth(name),
                             rating: Util.truncate(rating, 2),
                             naiveBest30: Util.truncate(getNaiveRating(30), 2),
                             naiveBest50: Util.truncate(getNaiveRating(50), 2),
--- src/geki/bests/best50.ts
+++ src/geki/bests/best50.ts
@@ -32,52 +32,6 @@ import { Chart } from "@maidraw/geki/chart";
 import * as kamaiTachi from "./lib/kamaiTachi";
 import { Util } from "@maidraw/lib/util";
 
-class HalfFullWidthConvert {
-    private static readonly charsets = {
-        latin: { halfRE: /[!-~]/g, fullRE: /[！-～]/g, delta: 0xfee0 },
-        hangul1: { halfRE: /[ﾡ-ﾾ]/g, fullRE: /[ᆨ-ᇂ]/g, delta: -0xedf9 },
-        hangul2: { halfRE: /[ￂ-ￜ]/g, fullRE: /[ᅡ-ᅵ]/g, delta: -0xee61 },
-        kana: {
-            delta: 0,
-            half: "｡｢｣､･ｦｧｨｩｪｫｬｭｮｯｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝﾞﾟ",
-            full:
-                "。「」、・ヲァィゥェォャュョッーアイウエオカキクケコサシ" +
-                "スセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワン゛゜",
-        },
-        extras: {
-            delta: 0,
-            half: "¢£¬¯¦¥₩\u0020|←↑→↓■°",
-            full: "￠￡￢￣￤￥￦\u3000￨￩￪￫￬￭￮",
-        },
-    };
-    // @ts-ignore
-    private static readonly toFull = (set) => (c) =>
-        set.delta
-            ? String.fromCharCode(c.charCodeAt(0) + set.delta)
-            : [...set.full][[...set.half].indexOf(c)];
-    // @ts-ignore
-    private static readonly toHalf = (set) => (c) =>
-        set.delta
-            ? String.fromCharCode(c.charCodeAt(0) - set.delta)
-            : [...set.half][[...set.full].indexOf(c)];
-    // @ts-ignore
-    private static readonly re = (set, way) =>
-        set[way + "RE"] || new RegExp("[" + set[way] + "]", "g");
-    private static readonly sets = Object.values(this.charsets);
-    // @ts-ignore
-    static toFullWidth = (str0) =>
-        this.sets.reduce(
-            (str, set) => str.replace(this.re(set, "half"), this.toFull(set)),
-            str0
-        );
-    // @ts-ignore
-    static toHalfWidth = (str0) =>
-        this.sets.reduce(
-            (str, set) => str.replace(this.re(set, "full"), this.toHalf(set)),
-            str0
-        );
-}
-
 interface ITheme {
     manifest: IThemeManifest;
     path: string;
@@ -998,7 +952,7 @@ export class Best50 {
 
             Util.drawText(
                 ctx,
-                HalfFullWidthConvert.toFullWidth(username),
+                Util.HalfFullWidthConvert.toFullWidth(username),
                 element.x + theme.manifest.width * (7 / 32) * (89 / 128),
                 element.y + theme.manifest.width * (7 / 32) * (49 / 128),
                 (theme.manifest.width * (7 / 32) * 1) / 8,
@@ -1200,7 +1154,7 @@ export class Best50 {
                                 const scoreRating = bestScores
                                     .slice(0, 60)
                                     .map((v) => v.rating)
-                                    .reduce((sum, v) => (sum += v));
+                                    .reduce((sum, v) => sum + v);
                                 const starRating = recentOrPlatinumScores
                                     .slice(0, 50)
                                     .map((v) => {
@@ -1227,7 +1181,7 @@ export class Best50 {
                                             ) / 1000
                                         );
                                     })
-                                    .reduce((sum, v) => (sum += v));
+                                    .reduce((sum, v) => sum + v, 0);
                                 return (
                                     Util.truncateNumber(scoreRating / 50, 3) +
                                     Util.truncateNumber(starRating / 50, 3)
@@ -1245,11 +1199,12 @@ export class Best50 {
                                 scores
                                     .slice(0, length)
                                     .map((v) => v.rating)
-                                    .reduce((sum, v) => (sum += v)) / length
+                                    .reduce((sum, v) => sum + v, 0) / length
                             );
                         }
                         await this.drawTextModule(ctx, currentTheme, element, {
-                            username: HalfFullWidthConvert.toFullWidth(name),
+                            username:
+                                Util.HalfFullWidthConvert.toFullWidth(name),
                             rating: Util.truncate(
                                 rating,
                                 options.type == "refresh" ? 3 : 2
