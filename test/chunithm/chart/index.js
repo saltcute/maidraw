5f34c0f58d7e87aa3e4e08b94fdfa312f80bcdb5
potatopotat0 <xuanjiap@lolicon.ac.cn>
2025-08-24 22:42:55 +0800

    chore: test script

---
+++ test/chunithm/chart/index.js
@@ -0,0 +1,45 @@
+const sharp = require("sharp");
+const upath = require("upath");
+
+(async () => {
+    const { MaiDraw } = require("../../../dist");
+    const kamai = new MaiDraw.Chuni.Best50.KamaiTachi();
+    const lxns = new MaiDraw.Chuni.Best50.LXNS({
+        auth: "",
+    });
+    MaiDraw.Chuni.Chart.Database.setLocalDatabasePath(
+        "../maimai-songs-database"
+    );
+
+    const fs = require("fs");
+    const themes = ["jp-verse"];
+    for (let theme of themes) {
+        for (const region of ["JPN"]) {
+            let result = await MaiDraw.Chuni.Chart.drawWithScoreSource(
+                kamai,
+                "tluo5458",
+                428,
+                "recents",
+                {
+                    scale: 1,
+                    theme,
+                    region,
+                }
+            );
+            if (result) {
+                fs.writeFileSync(
+                    upath.join(__dirname, `${theme}-${region}.webp`),
+                    await sharp(result)
+                        .webp({
+                            quality: 60,
+                        })
+                        .toBuffer()
+                );
+                console.log(`${theme} passed.`);
+            } else {
+                console.log(`${theme} failed!`);
+            }
+        }
+    }
+    process.exit(0);
+})();
