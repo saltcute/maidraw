f25b4b5b4825d359a6c1e08719223dee28ec8546
saltcute <me@mizuno.ai>
2025-10-04 04:36:35 -0400

    feat: support combo and sync display and getPlayerScore in maishift adapter

--- src/mai/lib/adapter/maishift/index.ts
+++ src/mai/lib/adapter/maishift/index.ts
@@ -7,6 +7,7 @@ import {
     ESyncTypes,
     EComboTypes,
     EAchievementTypes,
+    EDifficulty,
 } from "@maidraw/mai/type";
 import { Database } from "@maidraw/mai/lib/database";
 
@@ -16,58 +17,120 @@ export class Maishift extends ScoreTrackerAdapter {
             baseURL: "https://maimai.shiftpsh.com",
         });
     }
-    private async pageScraper(username: string) {
-        const html = await this.get<string>(`/profile/${username}/home`);
-        if (!html) return null;
-        const $ = Cheerio.load(html.replace(/<style.*?>.*?<\/style>/g, ""));
-
-        const name = $("body > div > div > div > div > span > span").text();
-        const avatarUrl = $($("body > div > div > img")[0]).attr("src");
-        const title = $($("body > div > div > div > div > div")[0]).text();
+    private readonly CURRENT_MINOR = 55;
+    private readonly CURRENT_ACCUMULATE_VER = 24;
+    private minorToAccumulateVer(minor: number, isDX: boolean) {
+        if (isDX) {
+            if (minor >= 55) return 24;
+            if (minor >= 50) return 23;
+            if (minor >= 45) return 22;
+            if (minor >= 40) return 21;
+            if (minor >= 35) return 20;
+            if (minor >= 30) return 19;
+            if (minor >= 25) return 18;
+            if (minor >= 20) return 17;
+            if (minor >= 15) return 16;
+            if (minor >= 10) return 15;
+            if (minor >= 5) return 14;
+            return 13;
+        } else {
+            if (minor >= 97) return 12;
+            if (minor >= 95) return 11;
+            if (minor >= 90) return 10;
+            if (minor >= 85) return 9;
+            if (minor >= 80) return 8;
+            if (minor >= 70) return 7;
+            if (minor >= 60) return 6;
+            if (minor >= 50) return 5;
+            if (minor >= 40) return 4;
+            if (minor >= 30) return 3;
+            if (minor >= 20) return 2;
+            if (minor >= 10) return 1;
+            return 0;
+        }
+    }
+    private async profileScraper(username: string) {
+        const HTML = await this.get<string>(
+            `/profile/${username}/home`,
+            undefined,
+            1 * 60 * 1000
+        );
+        if (!HTML) return null;
+        const $ = Cheerio.load(HTML.replace(/<style.*?>.*?<\/style>/g, ""));
+        const profileElement = $("> div", $("body > div")[0])[2];
+        const name = $($("> div > span", $("> div", profileElement))[0]).text();
+        const avatarUrl = $("> img", profileElement).attr("src");
+        const title = $($("> div", $("> div", profileElement))[0]).text();
         const rating = parseInt(
-            $($("body > div > div > div > div > div")[1]).text()
+            $($("> div > div", $("> div", profileElement))[1]).text()
+        );
+        if (!name || isNaN(rating)) return null;
+        return { name, avatarUrl, title, rating };
+    }
+    private getScoresFromDOM($: Cheerio.CheerioAPI, length: number) {
+        const DOM = $("body > div > div").slice(15, 15 + length);
+        const scores = [];
+        for (const score of DOM) {
+            const level = parseFloat(
+                $($("> div", score)[0]).text().replace("+", "")
+            );
+            const name = $($("> span", $("> div", score)[1])[0]).text();
+            const rank = $($("> div", score)[2]).text();
+            const combo = $($("> div", score)[3]).text();
+            const sync = $($("> div", score)[4]).text();
+            const rating = parseInt(
+                $($("> span", $("> div", score)[5])[1]).text()
+            );
+            const rate = parseInt(
+                $($("> span", $("> div", score)[5])[0])
+                    .text()
+                    .replace(".", "")
+                    .replace("%", "")
+            );
+            const isDX =
+                $($("> img", $("> div", $("> div", score)[1])[0])[0]).attr(
+                    "alt"
+                ) == "DX";
+            if (name && level && !isNaN(rate) && !isNaN(rating) && rank)
+                scores.push({
+                    rank,
+                    level,
+                    rating,
+                    name,
+                    rate,
+                    combo,
+                    sync,
+                    isDX,
+                });
+        }
+        return scores;
+    }
+    private async best50Scraper(username: string) {
+        const scoresHTML = await this.get<string>(
+            `/profile/${username}/records?&version=${this.CURRENT_ACCUMULATE_VER}&sort=rating&order=desc`,
+            undefined,
+            1 * 60 * 1000
+        );
+        const oldScoresHTML = await this.get<string>(
+            `/profile/${username}/records?&version=${encodeURI(Array.from(Array(this.CURRENT_ACCUMULATE_VER).keys()).join(","))}&sort=rating&order=desc`,
+            undefined,
+            1 * 60 * 1000
+        );
+        if (!scoresHTML || !oldScoresHTML) return null;
+        const $new = Cheerio.load(
+            scoresHTML.replace(/<style.*?>.*?<\/style>/g, "")
+        );
+        const $old = Cheerio.load(
+            oldScoresHTML.replace(/<style.*?>.*?<\/style>/g, "")
         );
 
-        const newScoresDOM = $($("body > div > div")[14]).children();
-        const newScores = [];
-        for (const score of newScoresDOM) {
-            const rank = $($("div", score)[2]).text();
-            const level = parseFloat(
-                $($("div", score)[6]).text().replace("+", "")
-            );
-            const rating = parseInt($($("span", $("div", score)[5])[0]).text());
-            const name = $($("div", score)[8]).text();
-            const rate = parseFloat(
-                $($("span", $("div", score)[9])[0])
-                    .text()
-                    .replace("%", "")
-            );
-            const isDX = $($("img", score)[1]).attr("alt") == "DX";
-            newScores.push({ rank, level, rating, name, rate, isDX });
-        }
+        const newScores = this.getScoresFromDOM($new, 15);
+        const oldScores = this.getScoresFromDOM($old, 35);
 
-        const oldScoresDOM = $($("body > div > div")[16]).children();
-        const oldScores = [];
-        for (const score of oldScoresDOM) {
-            const rank = $($("div", score)[2]).text();
-            const level = parseFloat(
-                $($("div", score)[6]).text().replace("+", "")
-            );
-            const rating = parseInt($($("span", $("div", score)[5])[0]).text());
-            const name = $($("div", score)[8]).text();
-            const rate = parseFloat(
-                $($("span", $("div", score)[9])[0])
-                    .text()
-                    .replace("%", "")
-            );
-            const isDX = $($("img", score)[1]).attr("alt") == "DX";
-            oldScores.push({ rank, level, rating, name, rate, isDX });
-        }
-
-        return { name, avatarUrl, title, rating, newScores, oldScores };
+        return { newScores, oldScores };
     }
     async getPlayerInfo(username: string) {
-        const res = await this.pageScraper(username);
+        const res = await this.profileScraper(username);
         if (!res) return null;
         return {
             name: res.name,
@@ -75,7 +138,7 @@ export class Maishift extends ScoreTrackerAdapter {
         };
     }
     async getPlayerProfilePicture(username: string) {
-        const res = await this.pageScraper(username);
+        const res = await this.profileScraper(username);
         if (!res?.avatarUrl) return null;
         const buffer = await this.get<Buffer>(
             res.avatarUrl,
@@ -91,22 +154,82 @@ export class Maishift extends ScoreTrackerAdapter {
     async getPlayerBest50(
         username: string
     ): Promise<{ new: IScore[]; old: IScore[] } | null> {
-        const res = await this.pageScraper(username);
+        const res = await this.best50Scraper(username);
         if (!res) return null;
-        const newScores: IScore[] = [];
+        const scores: IScore[] = [];
         const oldScores: IScore[] = [];
         for (const score of res.newScores) {
             const converted = await this.toMaiDrawScore(score);
-            if (converted) newScores.push(converted);
+            if (converted) scores.push(converted);
         }
         for (const score of res.oldScores) {
             const converted = await this.toMaiDrawScore(score);
             if (converted) oldScores.push(converted);
         }
-        return { new: newScores, old: oldScores };
+        return { new: scores, old: oldScores };
     }
+    private async scoreScraper(
+        username: string,
+        chartId: number,
+        difficulty: EDifficulty
+    ) {
+        const chart = Database.getLocalChart(chartId, difficulty);
+        if (!chart) return null;
+        const addVersion = chart.addVersion.EX ?? chart.addVersion.DX;
+        if (!addVersion) return null;
+        const accumulateVer = this.minorToAccumulateVer(
+            addVersion?.gameVersion.minor,
+            chartId > 10000
+        );
+        const level =
+            (chart.events
+                .filter(
+                    (e) =>
+                        e.type == "existence" &&
+                        (e.version.region == "EX" || e.version.region == "DX")
+                )
+                .find((e) => e.version.gameVersion.minor >= this.CURRENT_MINOR)
+                ?.data.level as number) ?? chart.level;
+
+        const params = new URLSearchParams();
+        params.append("version", accumulateVer.toString());
+        params.append(
+            "level",
+            `${Math.trunc(level * 10 - 5)},${Math.trunc(level * 10 + 5)}`
+        );
+        params.append("chartType", chartId > 10000 ? "DX" : "STANDARD");
+        params.append(
+            "difficulty",
+            difficulty == EDifficulty.REMASTER
+                ? "RE_MASTER"
+                : EDifficulty[difficulty]
+        );
+        params.append("sort", "rating");
+        params.append("order", "desc");
+        const HTML = await this.get<string>(
+            `/profile/${username}/records?${params.toString()}`,
+            undefined,
+            1 * 60 * 1000
+        );
+        if (!HTML) return null;
+        const $ = Cheerio.load(HTML.replace(/<style.*?>.*?<\/style>/g, ""));
+        const scores = this.getScoresFromDOM($, 100);
+        return (
+            scores.find(
+                (s) => s.name == chart.name && s.isDX == chartId > 10000
+            ) || null
+        );
+    }
+
     async getPlayerScore(username: string, chartId: number) {
-        return {
+        const res: {
+            basic: IScore | null;
+            advanced: IScore | null;
+            expert: IScore | null;
+            master: IScore | null;
+            remaster: IScore | null;
+            utage: IScore | null;
+        } = {
             basic: null,
             advanced: null,
             expert: null,
@@ -114,6 +237,22 @@ export class Maishift extends ScoreTrackerAdapter {
             remaster: null,
             utage: null,
         };
+        for (
+            let difficulty = EDifficulty.BASIC;
+            difficulty <= EDifficulty.REMASTER;
+            difficulty++
+        ) {
+            const score = await this.scoreScraper(
+                username,
+                chartId,
+                difficulty
+            );
+            if (score) {
+                res[EDifficulty[difficulty].toLowerCase() as keyof typeof res] =
+                    await this.toMaiDrawScore(score);
+            }
+        }
+        return res;
     }
     async toMaiDrawScore(score: {
         rank: string;
@@ -121,6 +260,8 @@ export class Maishift extends ScoreTrackerAdapter {
         rating: number;
         name: string;
         rate: number;
+        combo: string;
+        sync: string;
         isDX: boolean;
     }): Promise<IScore | null> {
         const chart = await Database.findLocalChartWithNameAndLevel(
@@ -137,10 +278,37 @@ export class Maishift extends ScoreTrackerAdapter {
                 level: chart.level,
                 maxDxScore: chart.meta.maxDXScore,
             },
-
-            combo: EComboTypes.NONE,
-            sync: ESyncTypes.NONE,
-            achievement: score.rate,
+            combo: (() => {
+                switch (score.combo) {
+                    case "FC":
+                        return EComboTypes.FULL_COMBO;
+                    case "FC+":
+                        return EComboTypes.FULL_COMBO_PLUS;
+                    case "AP":
+                        return EComboTypes.ALL_PERFECT;
+                    case "AP+":
+                        return EComboTypes.ALL_PERFECT_PLUS;
+                    default:
+                        return EComboTypes.NONE;
+                }
+            })(),
+            sync: (() => {
+                switch (score.sync) {
+                    case "SYNC":
+                        return ESyncTypes.SYNC_PLAY;
+                    case "FS":
+                        return ESyncTypes.FULL_SYNC;
+                    case "FS+":
+                        return ESyncTypes.FULL_SYNC_PLUS;
+                    case "FDX":
+                        return ESyncTypes.FULL_SYNC_DX;
+                    case "FDX+":
+                        return ESyncTypes.FULL_SYNC_DX_PLUS;
+                    default:
+                        return ESyncTypes.NONE;
+                }
+            })(),
+            achievement: score.rate / 10000,
             achievementRank: (() => {
                 switch (score.rank) {
                     case "SSS+":
