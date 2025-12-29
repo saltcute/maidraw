3343e59c84d25a9316c0246782d0b9dbfeb1ca8e
saltcute <me@mizuno.ai>
2025-12-29 04:42:41 -0500

    fix: export BaseScoreAdapter

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,12 @@ import * as geki from "./geki";
 import * as chu from "./chu";
 import * as mai from "./mai";
 
+import * as adapter from "./lib/adapter";
+
 export namespace MaiDraw {
     export import Maimai = mai.Maimai;
     export import Chuni = chu.Chuni;
     export import Geki = geki.Geki;
+
+    export import BaseScoreAdapter = adapter.BaseScoreAdapter;
 }
