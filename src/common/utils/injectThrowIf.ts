d827f7c89752088f8845227235668892dc6eb95b
saltcute <me@mizuno.ai>
2026-07-10 00:54:28 -0400

    chore: formatting

--- src/common/utils/injectThrowIf.ts
+++ src/common/utils/injectThrowIf.ts
@@ -1,9 +1,9 @@
 /**
  * A dirty workaround to disable all score validity check in rg-stats.
  * Used so that the code won't be unnecessarily complicated just to simply calculate a rating.
- * 
+ *
  * Eg. 101.0000% score without a ALL PERFECT+ lamp.
- * 
+ *
  * TODO: a better way to handle this???
  */
 
